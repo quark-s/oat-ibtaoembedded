a485c99992cffaa12d73c75a690bfc3747dcf075
Felix Wagner <quark_s@gmx.net>
2022-12-03 01:33:07 +0100

    confDefault => tmp

--- scripts/packer/index.js
+++ scripts/packer/index.js
@@ -23,10 +23,6 @@ const argv = yargs(hideBin(process.argv))
   .nargs("o", 1)
   .describe("o", "Output directory (default: current directory)").argv;
 
-let item = null;
-let confFile = "../../views/js/pciCreator/ibTaoEmbedded/confDefault.json";
-let conf = JSON.parse(fs.readFileSync(confFile));
-
     function getFileList(_path, _relativeRoot){
         let tmp = [];
         fs.readdirSync(_path, ).forEach(f => {
@@ -60,6 +56,9 @@ let conf = JSON.parse(fs.readFileSync(confFile));
     fs.mkdirSync("./tmp");
     fs.copySync(relativeRoot, "./tmp");
 
+	let item = null;
+	let confFile = "./tmp/confDefault.json";
+	let conf = JSON.parse(fs.readFileSync(confFile));
 
 // ******** replace identifier *********
 
